c6f2b72ecdf803f5759bd3785f6d03c7acf2d1a9
cw1 <c.witt.1900@gmail.com>
2017-02-19 21:53:25 +0100

    Update provision.bash

--- provision.bash
+++ provision.bash
@@ -7,5 +7,6 @@ tar zxf /root/jira.tar.gz --strip=1 -C /opt/jira
 rm /root/jira.tar.gz
 chown -R jira:jira /opt/atlassian-home
 echo "jira.home = /opt/atlassian-home" > /opt/jira/atlassian-jira/WEB-INF/classes/jira-application.properties
+echo "JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> /opt/jira/bin/setenv.sh
 chown -R jira:jira /opt/jira
 mv /opt/jira/conf/server.xml /opt/jira/conf/server-backup.xml
